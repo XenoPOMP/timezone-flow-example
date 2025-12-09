edfe7b2c9244b227b9770dc01ff66141d98c1b21
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-12-09 16:16:18 +0200

    📂 change env

--- timezone-flow-frontend/src/utils/env.ts
+++ timezone-flow-frontend/src/utils/env.ts
@@ -1,11 +1,16 @@
 import { z } from 'zod';
 
 // Schema of env parsed object
-const envSchema = z.object({
-  CANONICAL_URL: z.string().default('http://localhost:3000'),
-  API_URL: z.string(),
-  NODE_ENV: z.string().optional(),
-});
+const envSchema = z
+  .object({
+    CANONICAL_URL: z.string().default('http://localhost:3000'),
+    NEXT_PUBLIC_API_URL: z.string(),
+    NODE_ENV: z.string().optional(),
+  })
+  .transform(({ NEXT_PUBLIC_API_URL, ...rest }) => ({
+    ...rest,
+    API_URL: NEXT_PUBLIC_API_URL,
+  }));
 
 export type EnvSchema = z.infer<typeof envSchema>;
 
