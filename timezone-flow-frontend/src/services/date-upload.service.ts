5721efb57492aff535e05a881dc2c3447242a01b
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2025-12-09 16:08:39 +0200

    ⚙️ modify the service

--- timezone-flow-frontend/src/services/date-upload.service.ts
+++ timezone-flow-frontend/src/services/date-upload.service.ts
@@ -1,3 +1,11 @@
+import { axiosForApi } from '@/api';
 import type { AnyService } from '@/types';
 
-const DateUploadService = {} satisfies AnyService;
+/* eslint-disable jsdoc/require-jsdoc */
+/**
+ * Main service for uploading date string.
+ */
+const DateUploadService = {
+  upload: () => axiosForApi.post('/upload', { dateString: '' }),
+} satisfies AnyService;
+/* eslint-enable jsdoc/require-jsdoc */
